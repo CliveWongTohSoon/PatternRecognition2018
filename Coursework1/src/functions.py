017fd391f57b05ca2835629d3c145dd90c1b3960
clivewong21 <xtwc32@live.com>
2018-11-13 15:18:19 +0000

    Clean up the code

--- Coursework1/src/functions.py
+++ Coursework1/src/functions.py
@@ -2,8 +2,6 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
 
 def select_image_by_label(l: int, label: list, image: list):
     col_indices = np.where(label == l)
@@ -22,7 +20,7 @@ def plot_all_faces(faces):
     for i in range(n):
         plt.subplot(n_of_rows, 2, i+1), plot_face(faces[:, i])
 
-def compute_avg_face(face_list, axis=1) -> np: 
+def compute_avg_face(face_list, axis=1): 
     face_avg = np.mean(face_list, axis=axis)
     return face_avg
 
@@ -46,61 +44,5 @@ def compute_s_w(face, face_label, face_avg):
     for key, table in grouped:
         x_m_i = table - grouped_mean.loc[key, :]
         np_xmi = np.dot(x_m_i.values.T, x_m_i.values)
-        list_x.append(np_xmi)
-    
+        list_x.append(np_xmi)    
     return sum(list_x)
-
-def calc_accuracy(test_image, test_label, a, train_label, m_eigvecs, face_avg):
-    pred_label = list(map(lambda k: nn_classifier(a, train_label, m_eigvecs, face_avg, k), test_image.T))
-    acc = accuracy_score(test_label, pred_label)
-    cm = confusion_matrix(test_label, pred_label)
-    plt.matshow(cm, cmap="Blues")
-    plt.colorbar()
-    plt.ylabel('Actual')
-    plt.xlabel('Predicted')
-    return f"Accuracy: {acc:.2f}"
-
-# Classifier
-def nn_classifier(w_training, training_label, m_eigvecs, mean_image, face_image):
-    # map test image onto eigenspace
-    # normalize
-    phi = face_image - mean_image
-    # project on the eigenspace and represent the projection as w
-    w = np.dot(m_eigvecs.T, phi)
-    # calculate the distance 
-    dist = np.linalg.norm(w_training - w, axis=1) 
-    return training_label[np.argmin(dist)]
-
-def alternative_method(training_image, train_label, face_image):
-    df = pd.DataFrame(training_image.T, index=train_label)    
-    grouped_mean = df.groupby(df.index).mean()
-    grouped = df.groupby(df.index)
-
-    min_e = float('inf')
-    min_label = df.index[0]
-    for key, tab in grouped:
-        # Compute Principle (eigen) subspace per class
-        phi = tab - grouped_mean.loc[key, :]
-
-        A = phi.values.T
-        
-        D, N = A.shape
-        S = np.dot(A.T, A) / N
-        eigvals, eigvecs = np.linalg.eig(S)
-        u_i = A.dot(eigvecs)
-        norm_u_i = np.linalg.norm(u_i, axis=0)
-        u_i = u_i / norm_u_i
-
-        # Classification
-        face_mean = grouped_mean.loc[key, :].values
-        
-        phi = face_image - face_mean
-        
-        w = np.dot(phi.T, u_i)
-        x_n = face_mean + np.dot(u_i, w)
-
-        e = np.linalg.norm(face_image - x_n)
-        if e < min_e:
-            min_label = key
-            min_e = e
-    return min_label
---
+++ Coursework1/src/prediction.py
@@ -0,0 +1,77 @@
+import numpy as np
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from pca import calc_eig_pca_small
+from functions import compute_avg_face
+import matplotlib.pyplot as plt
+
+# def nn_classifier(training_image, train_label, k, face_image):
+#     # w_training, training_label, m_eigvecs, mean_image, face_image):
+#     # map test image onto eigenspace
+#     # normalize
+#     mean_image = compute_avg_face(training_image)
+#     m, N = training_image.shape
+#     eigvals, eigvecs = calc_eig_pca_small(training_image, m, N)
+#     k_eigvecs = eigvecs[:, :k]
+#     # Training
+#     phi_train = training_image - mean_image.reshape(m, 1)
+#     w_training = np.dot(k_eigvecs.T, phi_train)
+
+#     # project on the eigenspace and represent the projection as w
+#     phi_test = face_image - mean_image
+#     w = np.dot(k_eigvecs.T, phi_test)
+    
+#     # calculate the distance 
+#     dist = np.linalg.norm(w_training - w, axis=1) 
+#     return train_label[np.argmin(dist)]
+
+def nn_classifier(w_training, training_label, k_eigvecs, mean_image, face_image):
+    # map test image onto eigenspace
+    # normalize
+    phi = face_image - mean_image
+    # project on the eigenspace and represent the projection as w
+    w = np.dot(k_eigvecs.T, phi)
+    # calculate the distance 
+    dist = np.linalg.norm(w_training - w, axis=1) 
+    return training_label[np.argmin(dist)]
+
+def reconstruction_error_classifier(training_image, train_label, face_image):
+    df = pd.DataFrame(training_image.T, index=train_label)    
+    grouped = df.groupby(df.index)
+
+    min_e = float('inf')
+    min_label = df.index[0]
+    for key, tab in grouped:
+        # Compute Principle (eigen) subspace per class
+        train_image = tab.values.T
+        m, N = train_image.shape
+        eigvals, u_i = calc_eig_pca_small(train_image, m, N)
+        # Classification
+        face_mean = compute_avg_face(train_image)
+        
+        phi = face_image - face_mean
+        w = np.dot(phi.T, u_i)
+        x_n = face_mean + np.dot(u_i, w)
+
+        e = np.linalg.norm(face_image - x_n)
+        if e < min_e:
+            min_label = key
+            min_e = e
+    return min_label
+
+def calc_accuracy(pred_label, test_label):
+    acc = accuracy_score(test_label, pred_label)
+    cm = confusion_matrix(test_label, pred_label)
+    plt.matshow(cm, cmap="Blues")
+    plt.colorbar()
+    plt.ylabel('Actual')
+    plt.xlabel('Predicted')
+    return f"Accuracy: {acc:.2f}"
+
+def produce_pred_label(w_training, training_label, k_eigvecs, mean_image, test_image):
+    return list(map(lambda face_image: nn_classifier(w_training, training_label, k_eigvecs, mean_image, face_image), test_image.T))
+
+def produce_reconstruction_pred_label(train_image, train_label, test_image):
+    return list(map(lambda face_image: reconstruction_error_classifier(train_image, train_label, face_image), test_image.T))
+    
